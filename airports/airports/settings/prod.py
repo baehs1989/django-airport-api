ad82d85fd367e750c5f9197ee1b002f321fad2ce
HyungSoo Bae <hyungsoobae@HyungSooui-MacBook-Pro.local>
2018-08-25 17:49:03 -0400

    update prod for new database

--- airports/airports/settings/prod.py
+++ airports/airports/settings/prod.py
@@ -4,6 +4,12 @@ DEBUG = False
 
 ALLOWED_HOSTS = [".herokuapp.com"]
 
+
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+DATABASES['default']['CONN_MAX_AGE'] = 500
+
 # ttps://kirr.co/vklau5
 
 CORS_REPLACE_HTTPS_REFERER      = True
@@ -15,3 +21,4 @@ CSRF_COOKIE_SECURE              = True
 SECURE_HSTS_INCLUDE_SUBDOMAINS  = True
 SECURE_HSTS_SECONDS             = 1000000
 SECURE_FRAME_DENY               = True
+
